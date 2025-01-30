7bba1ee213cecd2fddf0c77ca3aa50259879c5ba
Boje <shakihiruko03@mail.com>
2025-01-30 12:58:24 +0100

    Push project to git

---
+++ src/main/java/com/example/habitmanager/HabitManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.habitmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitManagerApplication.class, args);
+    }
+
+}
