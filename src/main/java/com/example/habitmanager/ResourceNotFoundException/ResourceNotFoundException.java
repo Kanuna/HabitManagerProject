1b238e353777c348feafbd16de66b649bf353639
Boje <shakihiruko03@mail.com>
2025-01-30 18:06:09 +0100

    Can use swagger and connected to DB

---
+++ src/main/java/com/example/habitmanager/ResourceNotFoundException/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.habitmanager.ResourceNotFoundException;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
