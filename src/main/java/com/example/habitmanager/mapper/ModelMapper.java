781153c6738c71fdc83b487547cd305194dd4cf5
Boje <math039rrelev.techcollege.dk>
2025-02-13 14:42:21 +0100

    Added HabitCompleation

--- src/main/java/com/example/habitmanager/mapper/ModelMapper.java
+++ src/main/java/com/example/habitmanager/mapper/ModelMapper.java
@@ -4,6 +4,7 @@ import com.example.habitmanager.dto.*;
 import com.example.habitmanager.dtoCreate.*;
 import com.example.habitmanager.models.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ModelMapper {
@@ -14,6 +15,10 @@ public interface ModelMapper {
     HabitDTO toHabitDTO(Habit habit);
     Habit toHabit(HabitDTO habitDTO);
     HabitDTOCreate toHabitDTOCreate(Habit habit);
+
+    HabitCompletionDTO toHabitCompletionDTO(HabitCompletion habitCompletion);
+    HabitCompletion toHabitCompletion(HabitCompletionDTO HabitCompletionDTO);
+    HabitCompletionDTOCreate toHabitCompletionDTOCreate(HabitCompletion habitCompletion);
 
     StatsDTO toStatsDTO(Stats stats);
     Stats toStats(StatsDTO statsDTO);
